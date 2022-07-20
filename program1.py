b676f5fe9e682ffae7d1dddd1c330b87e23d098c
Salman Ahmad <salman.ahmad1@arbisoft.com>
2022-07-20 17:10:24 +0500

    deleted demo.txt file and made changes to print function

--- program1.py
+++ program1.py
@@ -2,7 +2,7 @@
 this is first program to counte the number of vowels, words, sentences, and punctuation marks.
 """
 import string
-def print_details(vowels,punctuations):
+def print_details(vowels,punctuations,words):
     """
     this function is used to print vowels and punctuations in a sentence
     Parameters:
@@ -13,6 +13,8 @@ def print_details(vowels,punctuations):
     """
     print("number of vowels = {}".format(vowels))
     print("number of punctuation marks  = {}".format(punctuations))
+    print("number of words = {}".format(len(words)))
+
 def count_vowels(test_sentence):
     """
     this function is used to count vowels
@@ -27,16 +29,18 @@ def count_vowels(test_sentence):
         if element in vowels:
             number_of_vowels=number_of_vowels+1
     return number_of_vowels
+
 def count_words(test_sentence):
     """
     this function is used to count total number of words
     Parameters:
             test_sentence (str): stores sentence set by user or data read from a file
     returns :
-            nothing
+            number_of_words (list) : contains list of words in a sentence
     """
-    number_of_word=test_sentence.split()
-    print( "number of word = {} ".format(len(number_of_word)))
+    number_of_words = test_sentence.split()
+    return number_of_words
+
 def count_puntuation_marks(sentence):
     """
     this function is used to count punctuation marks
@@ -50,6 +54,7 @@ def count_puntuation_marks(sentence):
         if element in string.punctuation:
             number_of_punctuation_marks+=1
     return number_of_punctuation_marks
+
 def main():
     """
     this is the main module of the program all global and defualt run is defined over here
@@ -61,15 +66,15 @@ def main():
         sentence = input("enter meaningful sentence")
         count_of_vowels = count_vowels(sentence)
         punctuation_marks = count_puntuation_marks(sentence)
-        print_details(count_of_vowels,punctuation_marks)
-        count_words(sentence)
+        words_list=count_words(sentence)
+        print_details(count_of_vowels,punctuation_marks,words_list)
     if choice==1:
         filename = input('enter filename with proper path like "D:\\myfiles\\welcome.txt"')
         file = open(filename.strip())
         content = file.read()
         count_of_vowels = count_vowels(content)
         punctuation_marks = count_puntuation_marks(content)
-        print_details(count_of_vowels,punctuation_marks)
-        count_words(content)
+        words_list = count_words(content)
+        print_details(count_of_vowels,punctuation_marks,words_list)
 if __name__ == "__main__":
     main()
